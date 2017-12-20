a7fdbafa7f8a9cf1682dd363ec932e6bee27d4a3
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2017-12-20 21:11:34 +0530

    Title change in index. Minor Visual issues fixed

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log("Server is running!");
+	console.log("Server is running!");
 });
